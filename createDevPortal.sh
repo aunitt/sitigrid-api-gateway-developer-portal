6f92fb1b2fe95a7d8cdd35e7a160395a0908bc7e
Ashley Unitt <ashley@unitt.org.uk>
2020-04-30 15:39:14 +0100

    Added script to create the dev portal

---
+++ createDevPortal.sh
@@ -0,0 +1,15 @@
+sam package --template-file ./cloudformation/template.yaml \
+    --output-template-file ./cloudformation/packaged.yaml \
+    --s3-bucket sitigrid-lambda-artifacts
+
+sam deploy --template-file ./cloudformation/packaged.yaml \
+    --stack-name "dev-portal" \
+    --s3-bucket sitigrid-lambda-artifacts \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+    DevPortalSiteS3BucketName="sitigrid-dev-portal-static-assets" \
+    ArtifactsS3BucketName="sitigrid-dev-portal-artifacts" \
+    CognitoDomainNameOrPrefix="sitigrid"
+
+aws cloudformation describe-stacks --query \
+    "Stacks[?StackName=='dev-portal'][Outputs[?OutputKey=='WebsiteURL']][][].OutputValue"
